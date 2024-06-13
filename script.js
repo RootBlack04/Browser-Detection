7702a63d04d030fdca39da418c359a7026db3616
Abdallah Torkamani <abdellahturkmani@gmail.com>
2024-06-13 13:53:05 +0100

    push Version 2

--- script.js
+++ script.js
@@ -21,6 +21,7 @@
 
 //---------------------------- Start here -------------------------------------------
 
+/* ------------------------- old version 1 ----------------------------------
 const userAgent = navigator.userAgent;
 let browserName, browserVersion;
 
@@ -67,3 +68,55 @@ browserNameElement.textContent =
 
 console.log(`Browser Name: ${browserName}`);
 console.log(`Browser Version: ${browserVersion}`);
+
+console.log(navigator);
+*/
+
+const userAgent = navigator.userAgent;
+let browserName, browserVersion;
+
+if (userAgent.match(/edg/i)) {
+  browserName = "Edge";
+  browserVersion = userAgent.match(/edg\/(\d+)/i)
+    ? userAgent.match(/edg\/(\d+)/i)[1]
+    : "";
+} else if (userAgent.match(/opr|opera/i)) {
+  browserName = "Opera";
+  browserVersion = userAgent.match(/opr\/(\d+)/i)
+    ? userAgent.match(/opr\/(\d+)/i)[1]
+    : "";
+} else if (userAgent.match(/chrome|chromium|crios/i)) {
+  browserName = "Chrome";
+  browserVersion = userAgent.match(/chrome\/(\d+)/i)
+    ? userAgent.match(/chrome\/(\d+)/i)[1]
+    : "";
+} else if (userAgent.match(/firefox|fxios/i)) {
+  browserName = "Firefox";
+  browserVersion = userAgent.match(/firefox\/(\d+)/i)
+    ? userAgent.match(/firefox\/(\d+)/i)[1]
+    : "";
+} else if (userAgent.match(/safari/i)) {
+  browserName = "Safari";
+  browserVersion = userAgent.match(/version\/(\d+)/i)
+    ? userAgent.match(/version\/(\d+)/i)[1]
+    : "";
+} else if (userAgent.match(/trident/i)) {
+  browserName = "Internet Explorer";
+  browserVersion = userAgent.match(/rv:(\d+)/i)
+    ? userAgent.match(/rv:(\d+)/i)[1]
+    : "";
+} else {
+  browserName = "Other";
+  browserVersion = "Unknown";
+}
+
+const element = document.getElementById(browserName);
+element.classList.add("active");
+var browserNameElement = document.getElementById("browserName");
+browserNameElement.textContent =
+  "Your Browser is: " + browserName + ", And your Version: " + browserVersion;
+
+console.log(`Browser Name: ${browserName}`);
+console.log(`Browser Version: ${browserVersion}`);
+
+
